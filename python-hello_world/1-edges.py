238f9f7226c6786d3e28434aa7027ed651a1b97a
Ambrose <ambrose.matata11@gmail.com>
2023-07-26 19:43:15 +0300

    A script that prints string elements

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = "Hol"
-print("First 3 letters: {}".format(word_first_3))
 word_last_2 = "on"
-print("Last 2 letters: {}".format(word_last_2))
 middle_word = "olberto"
+print("Last 2 letters: {}".format(word_last_2))
+print("First 3 letters: {}".format(word_first_3))
 print("Middle word: {}".format(middle_word))
