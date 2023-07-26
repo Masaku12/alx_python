794036c1fccd30cd74924b062b93e0730b5620f9
Ambrose <ambrose.matata11@gmail.com>
2023-07-26 19:38:25 +0300

    Prints desired string elements

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -4,5 +4,5 @@ word_first_3 = "Hol"
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = "on"
 print("Last 2 letters: {}".format(word_last_2))
-middle_word = "bert"
+middle_word = "olbert0"
 print("Middle word: {}".format(middle_word))
