806110713efeb86bbf93dc93befb9b50986d130b
Ambrose <ambrose.matata11@gmail.com>
2023-07-28 18:20:51 +0300

    takes a number n as input and returns a list

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
+    fibonacci_list = [0, 1]
+    
+    for i in range(2):
+        next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_fibonacci)
+        
+    return fibonacci_list
