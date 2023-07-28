1e9806dc45c6d8bfe930cccd4d78f119c9291214
Ambrose <ambrose.matata11@gmail.com>
2023-07-28 18:28:55 +0300

    Checks Prime Number Function

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    
+    for num in range(2, int(number**0.5) + 1):
+        if number % num == 0:
+            return False
+    
+    return True
