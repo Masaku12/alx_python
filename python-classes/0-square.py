49747c2f96d0a1d357a604997654c4e10bfbd3a8
Ambrose <ambrose.matata11@gmail.com>
2023-08-14 22:55:25 +0300

    Added comments and docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 class Square:
+    """
+    This class defines a square by a private instance attribute 'size'.
+    
+    Attributes:
+        __size (int): The size of the square.
+    """
+    
     def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+        
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
