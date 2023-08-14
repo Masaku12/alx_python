0a7444e93a4a9445d53a9c344cb79f821bb1f6e5
Ambrose <ambrose.matata11@gmail.com>
2023-08-15 01:12:16 +0300

    accesses and updates private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,62 @@
+"""
+This module defines a square class
+"""
+
+class Square:
+    """
+    This class defines a square by a private instance attribute 'size' and provides methods
+    for retrieving the size and calculating the area.
+    
+    Attributes:
+        __size (int): The size of the square.
+    """
+    
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+        
+    @property
+    def size(self):
+        """
+        Retrieves the current size of the square.
+        
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+        
+        Args:
+            value (int): The new size to set.
+        
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def area(self):
+        """
+        Calculates and returns the current square area.
+        
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
