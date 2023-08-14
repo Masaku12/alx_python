db19acbaf5b7313f0dc51ecba3216eb0b7f8817d
Ambrose <ambrose.matata11@gmail.com>
2023-08-14 23:02:56 +0300

    defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -14,3 +14,21 @@ class Square:
             size (int): The size of the new square.
         """
         self.__size = size
+        
+    def get_size(self):
+        """
+        Retrieves the current size of the square.
+        
+        Returns:
+            int: The size of the square
+        """
+        return self.__size
+    
+    def set_size(self, new_size):
+        """
+        Sets a new size for the square.
+        
+        Args:
+            new_size (int): The new size to set.
+        """
+        self.__size = new_size
