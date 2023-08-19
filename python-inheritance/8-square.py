a459e15d8c2a4480be8f3cb23aa9cf7a5f2dc3a0
Ambrose <ambrose.matata11@gmail.com>
2023-08-19 16:07:08 +0300

    a class Square that inherits from class Rectangle

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -35,6 +35,50 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
         
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class being inherited from class BaseGeometry
+    
+    Args:
+        width: The width of the rectangle
+        height: The height of the rectangle
+    """
+    def __init__(self, width, height):
+        """
+        Initializes a rectangle instance with width and height
+        
+        Args:
+            width: The width of the rectangle
+            height: The height of the rectangle
+        """
+        
+        self.__width = 0 # initialized to 0
+        self.__height = 0 # initialized to 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        
+    def area(self):
+        """
+        Calculate area of the rectangle
+        
+        Returns:
+            int: Area of the rectangle
+        """
+        
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """
+        Obtain a string that represents the rectangle
+        
+        Returns:
+            str: a formated string
+        """
+        
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        
 class Square(Rectangle):
     """
     A square class that inherits from the Rectangle class
