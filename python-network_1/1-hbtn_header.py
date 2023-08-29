fbb825aed3d677f4411edd49046406294097e171
Ambrose <ambrose.matata11@gmail.com>
2023-08-29 23:08:01 +0300

    Response header value #1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+# Importing the relevant 
+import requests
+import sys
+
+# Get the url from command-line argument
+url = sys.argv[1]
+
+# Send an HTTP GET request to the intended url
+response = requests.get(url)
+
+# Check if the response has the 'X-Request-Id' header
+if 'X-Request-Id' in response.headers:
+    # Extract the value of the 'X-Requesr-Id' header from the response
+    request_id = response.headers['X-Request-Id']
+    print("X-Request-Id.", request_id)
+else:
+    print("X-Request-Id header not found in the response.")
