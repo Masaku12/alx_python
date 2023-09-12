01a815ab981f77c5fb11394a13fbd21fb76b8b92
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-13 00:24:33 +0300

    lists all states starting with a N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,18 @@
 import MySQLdb
 import sys
 
+
 # Define a function that lists states with names starting with 'N'
 def list_states_with_N(mysqlusername, mysqlpassword, mysqldatabase):
     try:
         # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=mysqlusername, passwd=mysqlpassword, db=mysqldatabase)
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
         cursor = db.cursor()
 
         # Execute the SQL query to fetch states starting with "N"
@@ -24,15 +31,16 @@ def list_states_with_N(mysqlusername, mysqlpassword, mysqldatabase):
         cursor.close()
         db.close()
 
+
 # Check if the script is being run as the main program
 if __name__ == "__main__":
     # Check if the accurate number of cmd-line args is provided
     if len(sys.argv) != 4:
-        print("Usage: 1-filter_states.py <mysqlusername> <mysqlpassword> <mysqldatabase>")
+        print("Usage: 1-filter_states.py <username> <password> <database>")
         sys.exit(1)
 
     # Extract cmd-line args
-    mysqlusername, mysqlpassword, mysqldatabase = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Call the function to list states starting with 'N'
-    list_states_with_N(mysqlusername, mysqlpassword, mysqldatabase)
+    list_states_with_N(username, password, database)
