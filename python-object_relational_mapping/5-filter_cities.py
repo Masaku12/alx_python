eb3db50bc5a29b1ae322809356f2ee509eecfeed
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-14 18:45:04 +0300

    this script lists all cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Check if the number of cmd-line args is given
+    if len(sys.argv) != 5:
+        print("Usage: {}".format(sys.argv[1:5]))
+        sys.exit(1)
+
+    # Extract the cmd-line args
+    username, password, database, state_name = sys.argv[1:5]
+
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect (
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        # A cursor object that executes SQL queries
+        cursor = db.cursor()
+
+        # Define the SQL query with a parametized query
+        query = (
+            "SELECT cities.id, cities.name "
+            "FROM cities "
+            "INNER JOIN state ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id "
+        )
+
+        # Execute the query with state_name as a param
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the results from the executed query
+        results = cursor.fetchall()
+
+        # Display the results 
+        for row in results:
+            print(row)
+
+        # Close the cursor and db connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        # Handle SQL errors
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
