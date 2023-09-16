a182ef50a100301584de86ed99068f8b8369d7cb
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-16 10:46:15 +0300

    an empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,9 +4,4 @@ This module defines the base class
 
 
 class BaseGeometry:
-    """
-    This is a base class for geometry-related classes
-    """
-
-    def __init__(self):
-        pass
+    pass
