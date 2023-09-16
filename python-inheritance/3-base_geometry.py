7489e34dd4923e9749fb723bea240778bcae60f4
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-16 03:54:15 +0300

    fixed a class issue

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,10 +7,6 @@ class BaseGeometry:
     """
     This is a base class for geometry-related classes
     """
-    pass
 
-if __name__ == "__main__":
-    bg = BaseGeometry()
-    print(bg)
-    print(dir(bg))
-    print(dir(BaseGeometry))
+    def __init__(self):
+        pass
