a987ad5e7dd6feeae6eaee46e05640f7c606d1f6
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-16 03:08:05 +0300

    fixed class issues

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,8 +7,3 @@ class BaseGeometry:
     This is a base class for geometry-related classes
     """
     pass # an empty block
-
-bg = BaseGeometry()
-print(repr(bg))
-print(sorted(dir(bg)))
-print(sorted(dir(BaseGeometry)))
