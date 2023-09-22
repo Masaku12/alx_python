6a7c021287543f4ab3a79e705c570c91ed3b4931
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-22 22:06:03 +0300

    A script that starts a flask web app

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,32 @@
+"""
+A simple Flask web app that displays Hello HBNB
+"""
+
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the home page
+
+    Returns:
+        a str: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+def hbnb():
+    """
+    Route handler for the hbnb page
+
+    Returns:
+        a str: HBNB
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    # Start the web app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
