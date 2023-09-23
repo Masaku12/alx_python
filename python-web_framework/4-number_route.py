1b311585629d02bec3fa9291048da45defa08dc5
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-23 17:48:27 +0300

    checks whether input is an int

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,94 @@
+"""
+A simple Flask web app that displays Hello HBNB
+"""
+
+from flask import Flask, abort
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the home page
+
+    Returns:
+        a str: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+# Define a route for the /hbnb
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the hbnb page
+
+    Returns:
+        a str: HBNB
+    """
+    return "HBNB"
+
+# Define a route that accepts a dynamic parameter text
+@app.route('/c/<text>', strict_slashes=False)
+def custom_text(text):
+    """
+    Route handler for custom_text page
+
+    Args:
+        a str: The text provided as a param
+
+    Returns:
+        a str: "C " 
+    """
+    # Replace underscores with spaces in the text param
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+# Define a route that accepts a dynamic param 'text'
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Route handler for the /python/<text> page
+
+    Args:
+        text: a str
+
+    Returns:
+        a str: "Python " followed by value of text
+    """
+    formatted_text = text.replace('_', ' ')
+    return f"Python {formatted_text}"
+
+# Define a route that accepts an int param 'n'
+app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """
+    Route handler for the /number/<n> page
+
+    Args:
+        n (int): The int parameter
+
+    Returns:
+        str: "<n> is a num" if n is an int
+    """
+    return f"{n} is a number"
+
+# Handle invalid input for the /number/<n> route
+app.route('/number/<n>', strict_slashes=False)
+def invalid_number(n):
+    """
+    Route handler for invalid input page
+
+    Args:
+        n (str): The input provided as param
+
+    Returns:
+        str: An error message
+    """
+    abort(404)
+
+if __name__ == "__main__":
+    # Start the web app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
